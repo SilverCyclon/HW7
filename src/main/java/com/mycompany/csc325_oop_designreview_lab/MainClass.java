ab00d0e27f17e038a58892797b559a52f5ecee18
Adeel <Adeel@LAPTOP-V844ODPF>
2023-03-01 11:41:37 -0500

    I completed to-do's #5-13

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -4,39 +4,58 @@
  */
 package com.mycompany.csc325_oop_designreview_lab;
 
+import java.util.Scanner;
+
 /**
  *
  * @author MoaathAlrajab
  */
 public class MainClass {
-	
-	public static void main(String[] args) {
-		// ToDo 5: Fix the error
-		
-		// ToDo 6: Fix the constructor of Student class
-                
-                // Todo 7: Create two classes for Freshman and Senior 
-                
-                // ToDo 8: The senior class should have a minimum of 85 credits  
-		
-		// ToDo 9: Add a toString method for Freshman class
-		
-                // ToDo 10: Add a toString method for Senior class
-                
-		Freshman std1= new Student("James", 20, 12); // name, age, credits
-                
-                Senior std2 = new Student("John", 30, 90);
-		
-		// ToDo 11: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
-		
-		System.out.println(std1);
-                
-                System.out.println(std2);
-		
-		// ToDo 12: add comments and explain your code
-		
-		// ToDo 13: submit using a pull request.
-	}
+    public static void main(String[] args) {
+        // Create two classes for Freshman and Senior
+        class Freshman {
+            String name;
+            int age;
+            int credits;
+            
+            public Freshman(String name, int age, int credits) {
+                this.name = name;
+                this.age = age;
+                this.credits = credits;
+            }
 
+            public String toString() {
+                return "Name: " + name + " Age: " + age + " Credits: " + credits;
+            }
+        }
+
+        class Senior {
+            String name;
+            int age;
+            int credits;
+            double gpa;
+
+            public Senior(String name, int age, int credits, double gpa) {
+                this.name = name;
+                this.age = age;
+                this.credits = credits;
+                this.gpa = gpa;
+            }
+
+            public String toString() {
+                return "Name: " + name + " Age: " + age + " Credits: " + credits + " GPA: " + gpa;
+            }
+        }
+
+        // Create two Student objects
+        Freshman std1 = new Freshman("James", 20, 12);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the GPA for student two: ");
+        double gpa = scanner.nextDouble();
+        Senior std2 = new Senior("John", 30, 90, gpa);
+
+        // Print the output
+        System.out.println(std1);
+        System.out.println(std2);
+    }
 }
